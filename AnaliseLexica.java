8cb6b7e1d4077b12f4f2f6d0b207efc2f6b49b51
Victor Cosenza <vcvdsilva@inf.ufpel.edu.br>
2019-08-23 16:32:59 -0300

    Conversao para array de chars

--- AnaliseLexica.java
+++ AnaliseLexica.java
@@ -6,20 +6,15 @@ class Token{
   char lexema;
   TokenType token;
 
- Token (char l, TokenType t)
- 	{ lexema=l;token = t;}	
+ Token (char[] l, TokenType t)
+ 	{ lexema=l[0];token = t;}	
 
 }
 
 class AnaliseLexica {
-
 	BufferedReader arquivo;
-
-	AnaliseLexica(String a) throws Exception
-	{
-		
+	AnaliseLexica(String a) throws Exception {
 	 	this.arquivo = new BufferedReader(new FileReader(a));
-		
 	}
 
 	Token getNextToken() throws Exception
@@ -29,39 +24,55 @@ class AnaliseLexica {
 		char currchar;
 		int currchar1;
 
-			do{
+		char[] tokenContent;
+		String numberStream = "";
+
+			do {
 				currchar1 =  arquivo.read();
 				currchar = (char) currchar1;
 			} while (currchar == '\n' || currchar == ' ' || currchar =='\t' || currchar == '\r');
 			
-			if(currchar1 != eof && currchar1 !=10)
-			{
-								
-	
-				if (currchar >= '0' && currchar <= '9')
-					return (new Token (currchar, TokenType.NUM));
-				else
-					switch (currchar){
-						case '(':
-							return (new Token (currchar,TokenType.APar));
-						case ')':
-							return (new Token (currchar,TokenType.FPar));
-						case '+':
-							return (new Token (currchar,TokenType.SOMA));
-						case '*':
-							return (new Token (currchar,TokenType.MULT));
-						case '/':
-							return (new Token (currchar,TokenType.DIV));
-						case '-':
-							return (new Token (currchar,TokenType.SUB));
+			if(currchar1 != eof && currchar1 !=10) {	
+				arquivo.mark(2);
+				tokenContent = new char[1]; /* Dinamic Size */
+				tokenContent[0] = currchar;
+
+				if (currchar >= '0' && currchar <= '9'){
+					while (currchar >= '0' && currchar <= '9') {
+						arquivo.mark(2)
+						numberStream.append(currchar);
 						
-						default: throw (new Exception("Caractere inválido: " + ((int) currchar)));
+						currchar = (char) arquivo.read() /* Find next character */
 					}
+					arquivo.reset() /* Return 1 character */
+					return (new Token (tokenContent, TokenType.NUM));
+				}
+				else {
+					tokenContent = new char[1];
+					tokenContent[0] = currchar;
+					switch (tokenContent[0]) {
+						case '(':
+							return (new Token (tokenContent,TokenType.APar));
+						case ')':
+							return (new Token (tokenContent,TokenType.FPar));
+						case '+':
+							return (new Token (tokenContent,TokenType.SOMA));
+						case '*':
+							return (new Token (tokenContent,TokenType.MULT));
+						case '/':
+							return (new Token (tokenContent,TokenType.DIV));
+						case '-':
+							return (new Token (tokenContent,TokenType.SUB));
+						
+						default: throw (new Exception("Caractere inválido: " + ((int) tokenContent[0])));
+					}
+				}
 			}
 
-			arquivo.close();
-			
-		return (new Token(currchar,TokenType.EOF));
+		arquivo.close();
+		tokenContent = new char[1];
+		tokenContent[0] = currchar;	
+		return (new Token(tokenContent,TokenType.EOF));
 		
 	}
 }
