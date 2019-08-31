7a509cf770bf915b632f8adb7280265960599ec0
Victor <vcvdsilva@inf.ufpel.edu.br>
2019-08-31 17:50:53 -0300

    stack machine v1

--- stack_machine.hs
+++ stack_machine.hs
@@ -2,60 +2,77 @@ import Control.Monad
 import Data.List.Split
 import System.IO
 
-data Stack a = Stack [a] deriving (Show)
+data Stack = Stack [Integer] deriving (Show)
+
 
 data Binary = Add 
   | Sub 
   | Div 
   | Mult deriving (Show, Eq)
 
-binaryOp :: Binary -> Integer -> Integer -> Integer
-binaryOp Add = (+)
-binaryOp Sub = (-)
-binaryOp Div = quot
-binaryOp Mult = (*)
-
 data Instruction = Push Integer 
-  | Pop 
   | BinaryOp Binary  
   | Print
   | Halt deriving (Show)
-  
+
+
+binaryOp :: Binary -> Integer -> Integer -> Integer
+binaryOp Add = (+) 
+binaryOp Sub = (-)
+binaryOp Div = quot 
+binaryOp Mult = (*)
+
+
+push :: Integer -> Stack -> Stack
+push x (Stack xs) = Stack (x:xs)
+
+pop :: Stack -> (Stack, Integer)
+pop (Stack [])     = (Stack [], -1)
+pop (Stack (x:xs)) = (Stack xs, x)
+
 
 splitOnNewLine :: String -> [String]  
 splitOnNewLine = splitOn "\n"
 
+
 splitOnSpace :: String -> [String]  
 splitOnSpace = splitOn " "
+
 
 splitWords :: String -> [[String]]
 splitWords content = map splitOnSpace $ splitOnNewLine content
 
+
 lexemaToToken :: [String] -> Instruction
 lexemaToToken [lexema]
-  | lexema == "POP" = Pop
   | lexema == "SUM" = BinaryOp Add
   | lexema == "SUB" = BinaryOp Sub
   | lexema == "DIV" = BinaryOp Div
   | lexema == "MULT" = BinaryOp Mult
   | lexema == "PRINT" = Print
   | otherwise = Halt
-lexemaToToken [lexema, value] = Push (read value :: Integer)
+lexemaToToken ["PUSH", value] = Push (read value :: Integer)
 
 
+step :: Instruction -> Stack -> Stack
+step (Push value) stack           = push value stack  
+step (BinaryOp instruction) stack = push (binaryOp instruction num1 num2) stack'' 
+  where (stack', num1)            = pop (stack)
+        (stack'', num2)           = pop (stack')
+step _ (Stack s) = Stack s 
 
-compute :: [Instruction] -> Stack s -> Stack s
-compute [] (Stack s) = Stack s
-compute [_] (Stack []) = Stack []
+
+run :: [Instruction] -> Stack -> Stack
+run [] (Stack s) = Stack s
+run (Halt:_) (Stack s) = Stack s
+run (i:is) (Stack s)  = run is $ step i $ Stack s
+
+stackHead :: Stack -> Integer
+stackHead (Stack s) = head s
 
 main = do
   fileContent <- readFile "teste.out"
-  print $ "Step 0: " ++ fileContent ++ "\n"
-  
   let lexemas = splitWords fileContent
-  print "Step 1: \n"
-  print lexemas
-  
   let tokens = map lexemaToToken lexemas
-  print "Step 2: \n"
-  print tokens
+  let result = run tokens (Stack [])
+  print $ stackHead result
